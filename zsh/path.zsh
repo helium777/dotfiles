115cfb6529e97e75c41a3826537582d0e500e9e6
helium7 <helium747@gmail.com>
2024-11-18 14:06:17 +0800

    update PATH

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,5 +1,7 @@
 # required by fzf
 export PATH="$HOME/.fzf/bin:$PATH"
+# required by cargo
+export PATH="$HOME/.cargo/bin:$PATH"
 
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
