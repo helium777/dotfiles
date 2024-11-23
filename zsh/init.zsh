dbb5874f7147e03e7a8e4338bf5e50d5a800fe19
helium7 <helium747@gmail.com>
2024-11-24 01:40:48 +0800

    minor

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,13 +1,11 @@
 # init script for interactive shell sessions
 
-# set PATH
+source ~/.local/dotfiles/zsh/aliases.zsh
+source ~/.local/dotfiles/zsh/functions.zsh
+source ~/.local/dotfiles/zsh/history.zsh
 source ~/.local/dotfiles/zsh/path.zsh
+source ~/.local/dotfiles/zsh/plugins.zsh
+source ~/.local/dotfiles/zsh/styling.zsh
 
 # remove duplicates from PATH
 export PATH="$(perl -e 'print join(":", grep { not $seen{$_}++ } split(/:/, $ENV{PATH}))')"
-
-source ~/.local/dotfiles/zsh/history.zsh
-source ~/.local/dotfiles/zsh/functions.zsh
-source ~/.local/dotfiles/zsh/aliases.zsh
-source ~/.local/dotfiles/zsh/plugins.zsh
-source ~/.local/dotfiles/zsh/styling.zsh
