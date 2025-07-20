eb3074002fe8da4f76f433d5e20425c614361fcc
helium7 <helium747@gmail.com>
2025-07-20 16:30:22 +0800

    refactor bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,19 @@
-set -e
+#!/usr/bin/env bash
+set -euo pipefail
 
-# clone into ~/.local/dotfiles
-mkdir -p ~/.local
-cd ~/.local
-[ -d dotfiles ] || git clone https://github.com/helium777/dotfiles.git
+# clone into ~/.local/share/dotfiles
+mkdir -p ~/.local/share
+cd ~/.local/share
+[[ -d dotfiles ]] || git clone https://github.com/helium777/dotfiles.git
 cd dotfiles
 
 # update repo
 git pull
 
-# install nessessary packages
-bash install.sh
-
-# source init.zsh in the end of .zshrc
-perl -i -ne 'print unless /source ~\/.local\/dotfiles\/zsh\/init.zsh/' ~/.zshrc
-perl -i -pe 'print "source ~/.local/dotfiles/zsh/init.zsh\n" if $. == 1' ~/.zshrc
+# source init.zsh in the beginning of .zshrc
+if ! grep -q "source ~/.local/share/dotfiles/zsh/init.zsh" ~/.zshrc; then
+    temp_zshrc=$(mktemp)
+    printf "source ~/.local/share/dotfiles/zsh/init.zsh\n\n" > "$temp_zshrc"
+    [[ -f ~/.zshrc ]] && cat ~/.zshrc >> "$temp_zshrc"
+    mv "$temp_zshrc" ~/.zshrc
+fi
