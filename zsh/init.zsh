9f35d3b61668864d061bc9915d3860565bb750e2
helium7 <helium747@gmail.com>
2025-08-05 15:40:51 +0800

    refactor

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -3,11 +3,11 @@
 DOTFILES_HOME="${HOME}/.local/share/dotfiles"
 
 source "${DOTFILES_HOME}/zsh/aliases.zsh"
+source "${DOTFILES_HOME}/zsh/configs.zsh"
 source "${DOTFILES_HOME}/zsh/functions.zsh"
 source "${DOTFILES_HOME}/zsh/history.zsh"
 source "${DOTFILES_HOME}/zsh/path.zsh"
 source "${DOTFILES_HOME}/zsh/plugins.zsh"
-source "${DOTFILES_HOME}/zsh/styling.zsh"
 
 # remove duplicates from PATH
 export PATH="$(perl -e 'print join(":", grep { not $seen{$_}++ } split(/:/, $ENV{PATH}))')"
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,35 +1,43 @@
-### plugins managed by zinit
+# --- zinit ---
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
 [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 source "${ZINIT_HOME}/zinit.zsh"
 
-# prompt theme: pure
+# --- pure ---
 zinit ice compile'(pure|async).zsh' pick'async.zsh' src'pure.zsh'
 zinit light sindresorhus/pure
 
-# z.lua
+# --- z.lua ---
 zinit ice wait lucid
 zinit light skywind3000/z.lua
 
-# [install] fzf & activate it
+# --- fzf ---
 zinit ice wait lucid as"command" from"gh-r" \
     atclone"./fzf --zsh > init.zsh" \
     atpull"%atclone" src"init.zsh"
 zinit light junegunn/fzf
 
-# completion with fzf
+# --- fzf-tab ---
 zinit ice wait lucid atinit"zicompinit; zicdreplay"
 zinit light Aloxaf/fzf-tab
+# enable case-insensitive completion
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+# force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+zstyle ':completion:*' menu no
+# preview directory's content with eza when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza --group-directories-first --icons -1 --color=always $realpath'
 
-# syntax highlighting
+# --- fast-syntax-highlighting ---
 zinit ice wait lucid
 zinit light zdharma-continuum/fast-syntax-highlighting
 
-# auto suggest commands
-zinit ice wait lucid atload"_zsh_autosuggest_start"
+# --- zsh-autosuggestions ---
+zinit ice wait lucid atload"!_zsh_autosuggest_start"
 zinit light zsh-users/zsh-autosuggestions
+# first try the most recent command that follows the same context as current command, then search history, finally fallback to completion
+ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd history completion)
 
-# command completions
+# --- zsh-completions ---
 zinit ice wait lucid blockf atpull"zinit creinstall -q ."
 zinit light zsh-users/zsh-completions
