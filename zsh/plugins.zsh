3258837cc7609e31b6bf83af31e7f3e3127b41b6
helium7 <helium747@gmail.com>
2025-08-17 02:09:32 +0800

    fix

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -8,9 +8,19 @@ source "${ZINIT_HOME}/zinit.zsh"
 zinit ice compile'(pure|async).zsh' pick'async.zsh' src'pure.zsh'
 zinit light sindresorhus/pure
 
+# --- zsh-autosuggestions ---
+zinit ice wait lucid atload"_zsh_autosuggest_start"
+zinit light zsh-users/zsh-autosuggestions
+# first try the most recent command that follows the same context as current command, then search history, finally fallback to completion
+ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd history completion)
+
 # --- z.lua ---
 zinit ice wait lucid
 zinit light skywind3000/z.lua
+
+# --- gh ---
+zinit ice as"command" from"gh-r" mv"**/gh -> gh"
+zinit light cli/cli
 
 # --- fzf ---
 zinit ice wait lucid as"command" from"gh-r" \
@@ -19,29 +29,19 @@ zinit ice wait lucid as"command" from"gh-r" \
 zinit light junegunn/fzf
 
 # --- fzf-tab ---
-zinit ice wait lucid atinit"zicompinit; zicdreplay"
+zinit ice wait lucid
 zinit light Aloxaf/fzf-tab
-# enable case-insensitive completion and fuzzy matching
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' 'm:{a-z}={A-Z} r:|?=**'
+# enable case-insensitive completion and substring matching
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' 'l:|=* r:|=*'
 # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
 zstyle ':completion:*' menu no
 # preview directory's content with eza when completing cd
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza --group-directories-first --icons -1 --color=always $realpath'
 
-# --- fast-syntax-highlighting ---
-zinit ice wait lucid
-zinit light zdharma-continuum/fast-syntax-highlighting
-
-# --- zsh-autosuggestions ---
-zinit ice wait lucid atload"_zsh_autosuggest_start"
-zinit light zsh-users/zsh-autosuggestions
-# first try the most recent command that follows the same context as current command, then search history, finally fallback to completion
-ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd history completion)
-
 # --- zsh-completions ---
 zinit ice wait lucid blockf atpull"zinit creinstall -q ."
 zinit light zsh-users/zsh-completions
 
-# --- gh ---
-zinit ice as"command" from"gh-r" mv"**/gh -> gh"
-zinit light cli/cli
+# --- fast-syntax-highlighting ---
+zinit ice wait lucid atinit"zicompinit; zicdreplay"
+zinit light zdharma-continuum/fast-syntax-highlighting
