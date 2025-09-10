d41613c1ab1568667f3ba347d18e0f73d48a6d5c
helium7 <helium747@gmail.com>
2025-09-10 19:02:42 +0800

    update history options

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -2,8 +2,6 @@ HISTFILE=~/.zsh_history
 HISTSIZE=50000
 SAVEHIST=$HISTSIZE
 setopt extended_history
-setopt inc_append_history
-setopt share_history
 setopt hist_ignore_space
 setopt hist_ignore_dups
 setopt hist_reduce_blanks
